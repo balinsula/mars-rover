45489b71291a1d4bfe55ad15ff8aa695b6b70aad
balinsula <balii@pm.me>
2022-02-23 23:49:30 +0300

    feat: remove unused code

--- Utils/RandomExtensions.cs
+++ Utils/RandomExtensions.cs
@@ -2,19 +2,8 @@
 {
     public static class RandomExtensions
     {
-        // maximum number of uniform bits is 30 according to:
-        // https://stackoverflow.com/a/17080161/7032856
-        const int MaxNoOfUniformBitSize = 30;
-        const int UintBitSize = 32;
-        const int RemainderBitSize = UintBitSize - MaxNoOfUniformBitSize;
         public static Random Random = new Random();
 
-        public static uint GetNextUint()
-        {
-            var maxAvailableUniformBits = (uint)Random.Next(1 << MaxNoOfUniformBitSize);
-            var remainderBitsToComplete = (uint)Random.Next(1 << RemainderBitSize);
-            return (maxAvailableUniformBits << RemainderBitSize) | remainderBitsToComplete;
-        }
 
         public static T GetRandom<T>() where T : struct, Enum
         {
